35957e90c229164314944da642144af91cb9f8bd
Blair Anderson <blair81@gmail.com>
2013-08-29 22:15:33 -0600

    wow, i'm stupid. fixed the migration

--- db/migrate/20130830035952_remove_index_from_posts.rb
+++ db/migrate/20130830035952_remove_index_from_posts.rb
@@ -1,4 +1,5 @@
 class RemoveIndexFromPosts < ActiveRecord::Migration
   def change
+    remove_index :comments, :column => [:user_id, :post_id]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130827042953) do
+ActiveRecord::Schema.define(version: 20130830035952) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id",    null: false
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20130827042953) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id", "post_id"], name: "index_comments_on_user_id_and_post_id", unique: true
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "images", force: true do |t|
