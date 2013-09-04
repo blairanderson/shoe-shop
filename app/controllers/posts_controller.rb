42238a4fb1fedff4c6af329fea050243c5be489f
Blair Anderson <blair81@gmail.com>
2013-09-03 20:38:21 -0600

    skiping authenticity token for this paypal post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, except: [:new, :create, :index]
   before_action :require_login, except: [:show, :index, :callback]
   before_action :require_post_ownership, only: [:edit, :update, :destroy]
+  skip_before_filter :verify_authenticity_token, only: [:callback]
 
   def show
     impressionist(@post)
