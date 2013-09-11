3406c2f9ce1ddc9a2fd4ebfd1a45429ec4b1100e
Blair Anderson <blair81@gmail.com>
2013-09-10 22:55:34 -0600

    updating copy

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
 
   def post_title(post)
-    "#{post.title}, size: #{post.size.name} #{number_to_currency(post.price, precision: 0)}"
+    "#{post.brand} #{post.title}, size: #{post.size.name} #{number_to_currency(post.price, precision: 0)}"
   end
 end
