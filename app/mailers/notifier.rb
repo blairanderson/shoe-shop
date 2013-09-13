8eb4f38db80963e4ac19506f888e506b12028c6a
Blair Anderson <blair81@gmail.com>
2013-09-12 21:15:50 -0600

    added logo to signup email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,8 +6,8 @@ class Notifier < ActionMailer::Base
   # pass in the user object that contains the user's email address
 
   def signup_email(user)
+    attachments.inline['logo-black.png'] = File.read('app/assets/images/logo-black.png')
     @user = user
-    @logo_url = 
     mail( :to => user.email, :subject => 'Thanks for signing up' )
   end
 
