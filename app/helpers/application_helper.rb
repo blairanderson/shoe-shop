34f6d5253ccbd66ac37290d5b9ef12c6ad38ef20
Blair Anderson <blair81@gmail.com>
2013-09-12 20:40:15 -0600

    adding a logo. woot woot

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     content_for :title, content
   end
 
+  def white_logo
+    image_tag("logo-white.png", size: "100")
+  end
+
   def blog_helper_url
     begin
       "http://www.solesout.com#{Monologue::Post.last.posts_revisions.last.full_url}"
