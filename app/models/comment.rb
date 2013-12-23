ef128f1303e78c471e8425f78457311dacbd8b13
blair anderson <blair81@gmail.com>
2013-12-22 18:39:48 -0700

    watch a post after you comment on it.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,10 @@ class Comment < ActiveRecord::Base
   validates_presence_of :body
   validates_presence_of :post_id
   validates_presence_of :user_id
+
+  after_save :watch_the_post
+  def watch_the_post
+    WatchedItem.where( {post_id: self.post_id,user_id: self.user_id} ).first_or_create
+  end
+
 end
