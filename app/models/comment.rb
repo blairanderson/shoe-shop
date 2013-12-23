fc96f43f8476adfec79825d233e1252287a4b021
blair anderson <blair81@gmail.com>
2013-12-22 22:36:06 -0700

    notify all watchers except the commenter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,11 +15,11 @@ class Comment < ActiveRecord::Base
   def send_notifications
     post = self.post
     service = TCO.new
-    group = post.watchers.where.not(twitter: nil, id: self.user_id)
+    group = post.watchers.where.not(twitter: nil)
     group << post.user if post.user.twitter
     group.uniq
     group.each do |to|
-      service.update( to, post, self)
+      service.update( to, post, self) unless to.id == self.user_id
     end
   end
 end
