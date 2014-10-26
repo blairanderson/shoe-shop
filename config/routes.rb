9f597a5dc1b2a7bb0b52defbc3717b5a8d88ddc5
Blair Anderson <blair81@gmail.com>
2014-10-26 10:30:56 -0700

    route cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -42,15 +42,13 @@ ShoeShop::Application.routes.draw do
     end
   end
 
-  resources :password_resets
+  resources :password_resets, only: [:create, :edit, :update]
 
   resources :blog_posts, path: 'blog', only: [:show, :index] do
     collection do
       post 'webhook/:token' => 'blog_posts#webhook'
     end
   end
-
-
 
   get 'sitemap', to: 'pages#sitemap'
   get 'login', to: 'sessions#new'
