3d072db350a89e7157af44262f35a8e03736a145
Blair Anderson <blair81@gmail.com>
2014-11-01 15:32:27 -0700

    add validator for posts with images

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,20 +3,28 @@ class Post < ActiveRecord::Base
   acts_as_votable
   max_paginates_per 28
 
-  def update_scoreboard
-    board = {created_at: created_at,votes_up: cached_votes_up,votes_down: cached_votes_down}
-    result = Scoreboard.new(board).result
-    update scoreboard: result
-  end
+  alias_attribute :score, :cached_votes_score
+  alias_attribute :view_count, :impressions_count
+
+  before_validation :set_default_enum
 
   as_enum :status, [:draft, :for_sale, :sold, :deleted],
           whiny: false, column: 'status_enum'
 
-  validates :title,       presence: true, length: { maximum: 80 }
+  validates :user_id,     presence: true
+  validates :title,       presence: true, length: {maximum: 80}
   validates :price,       presence: true, numericality: true
   validates :body,        presence: true
   validates :scoreboard,  presence: true
   validates :status_enum, presence: true
+
+  validate :for_sale_images_count, if: :for_sale?
+  def for_sale_images_count
+    errors.add(:images_count, "Must have images to sell these shoes.") if images.count == 0
+  end
+
+
+
 
   belongs_to :user
   validates_associated :user, if: :user_id
@@ -30,17 +38,6 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :watched_items, dependent: :destroy
   has_many :watchers, through: :watched_items, source: :user
-
-  alias_attribute :score, :cached_votes_score
-  alias_attribute :view_count, :impressions_count
-
-  after_save :watch_the_post
-
-  def watch_the_post
-    WatchedItem.where(post_id: self.id, user_id: self.user_id).first_or_create
-  end
-
-  before_validation :set_default_enum
 
   def set_default_enum
     if status.nil?
@@ -56,21 +53,29 @@ class Post < ActiveRecord::Base
     self.send(sort)
   end
 
-  scope :drafted,   -> { where(status_enum: Post.draft) }
-  scope :active,    -> { where(status_enum: Post.for_sale) }
-  scope :inactive,  -> { where(status_enum: Post.sold) }
-  scope :removed,   -> { where(status_enum: Post.deleted) }
+  scope :with_images, -> { where.not(images_count: 0) }
+  scope :drafted, -> { where(status_enum: Post.draft) }
+  scope :active, -> { where(status_enum: Post.for_sale).with_images }
+  scope :inactive, -> { where(status_enum: Post.sold) }
+  scope :removed, -> { where(status_enum: Post.deleted) }
 
-  scope :top,     ->  { order(cached_votes_score: :desc, created_at: :desc) }
-  scope :bottom,  ->  { order(cached_votes_score: :asc, created_at: :desc) }
-  scope :oldest,  ->  { order(created_at: :asc) }
-  scope :newest,  ->  { order(created_at: :desc) }
-  scope :random,  ->  { order(scoreboard: :desc, created_at: :desc) }
+  scope :top, -> { order(cached_votes_score: :desc, created_at: :desc) }
+  scope :bottom, -> { order(cached_votes_score: :asc, created_at: :desc) }
+  scope :oldest, -> { order(created_at: :asc) }
+  scope :newest, -> { order(created_at: :desc) }
+  scope :random, -> { order(scoreboard: :desc, created_at: :desc) }
 
-  scope :sml,   ->  { where(size_id: [1, 2, 3, 4, 5, 6, 7]) } # 'nine-and-under'
-  scope :med,   ->  { where(size_id: [8,9]) }# 'nine5-to-10'
-  scope :lrg,   ->  { where(size_id: [10,11]) }# 'ten5-to-eleven'
-  scope :xl,    ->  { where(size_id: [12,13,14,15,16,17,18,19]) } # 'eleven5_plus'
+  scope :sml, -> { where(size_id: [1, 2, 3, 4, 5, 6, 7]) } # 'nine-and-under'
+  scope :med, -> { where(size_id: [8, 9]) } # 'nine5-to-10'
+  scope :lrg, -> { where(size_id: [10, 11]) } # 'ten5-to-eleven'
+  scope :xl, -> { where(size_id: [12, 13, 14, 15, 16, 17, 18, 19]) } # 'eleven5_plus'
+
+
+  def update_scoreboard
+    board = {created_at: created_at, votes_up: cached_votes_up, votes_down: cached_votes_down}
+    result = Scoreboard.new(board).result
+    update scoreboard: result
+  end
 
   def to_param
     "#{id} #{title}".parameterize
