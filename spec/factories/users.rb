ac5ac2d81d51502f608075b21397891d59196ba8
Blair Anderson <blair81@gmail.com>
2014-11-01 12:20:11 -0700

    fix user detail page and small refactors

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,14 +2,14 @@
 
 FactoryGirl.define do
   factory :user do
-    sequence(:username) { |n| "#{Faker::Company.catch_phrase.split.join("_")}#{n}"}
+    sequence(:username) { |n| "#{Faker::Company.catch_phrase.downcase.split.join("_")}#{n}"}
     sequence(:email)    { |n| "email+#{n}.#{Faker::Internet.email}" }
     password 'password'
   end
 
   factory :admin, class: User do
     user_name  "blair"
-    email "blair81@gmail.com"
+    email ENV["ADMINB"]
     password "password"
   end
 end
