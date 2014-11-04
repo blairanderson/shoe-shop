6b26e3a0443516e8b515c313e32562ee58aaadc9
Blair Anderson <blair81@gmail.com>
2014-11-03 21:16:54 -0800

    delete watched items

--- app/models/watched_item.rb
+++
@@ -1,9 +0,0 @@
-class WatchedItem < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :post
-
-  validates_presence_of :post_id
-  validates_presence_of :user_id
-
-  validates_uniqueness_of :user_id, scope: [:post_id], message: "Already Watching"
-end
