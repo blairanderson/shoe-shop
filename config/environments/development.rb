6d5c2c3cfbd85e4da90835f23b4f3acc041af33a
Blair Anderson <blair81@gmail.com>
2014-11-09 22:01:23 -0800

    update email template before resetting passwords

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ ShoeShop::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
